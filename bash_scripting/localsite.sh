f1f8d880972f7b935b8f69714fb9c40aac7bd60a
stolenpromises <nathan.ed.mcdonald@outlook.com>
2021-11-06 09:13:44 -0500

    Complete startup script for local host of site

---
+++ bash_scripting/localsite.sh
@@ -0,0 +1,3 @@
+# #!/bin/ bash  # specify interpreter to execute script
+cd /home/nathan_m/apps/stolenpromises.github.io/  # navigate to absolute dir
+bundle exec jekyll serve  # run the site locally
