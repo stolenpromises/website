ba51709fde0bdb905e3dc18d065afcc713945b28
stolenpromises <nathan.ed.mcdonald@outlook.com>
2021-11-14 15:52:25 -0600

    Deploy livereload

--- bash_scripting/localsite.sh
+++ bash_scripting/localsite.sh
@@ -1,4 +1,4 @@
 # #!/bin/ bash  # specify interpreter to execute script
 cd /home/nathan_m/apps/stolenpromises.github.io/  # navigate to absolute dir
 # bundle exec jekyll serve  # run the site locally
-bundle exec jekyll serve -livereload  # run the site locally with live reload
+bundle exec jekyll serve --livereload  # run the site locally with live reload
